e6e587eefb3d17a225c0fbaeb6589b22c797ba63
makiahtbennett@gmail.com <makiahtbennett@gmail.com>
2018-05-05 13:34:04 -0400

    Continued graphing improvements

--- app.py
+++ app.py
@@ -8,14 +8,16 @@ def Fy_function(x, y):
 	return y
 
 # The grid size, the arrows will be located on each of the grid points.  
-n = 8
-X, Y = np.mgrid[0:n, 0:n]
+WIDTH = 12
+HEIGHT = 8
+X, Y = np.mgrid[0:WIDTH, 0:HEIGHT]
 
 # Vector properties: r is color, T is angle
 # T = np.arctan2(Y - n / 2., X - n/2.)
 # R = 10 + np.sqrt((Y - n / 2.0) ** 2 + (X - n / 2.0) ** 2)
 # U, V = R * np.cos(T), R * np.sin(T)
-U, V = X, Y
+X_GRAPH, Y_GRAPH = X - (WIDTH - 1) / 2., Y - (HEIGHT - 1) / 2.
+U, V = X_GRAPH, Y_GRAPH
 R = np.sqrt(U**2+V**2)
 U, V = U / R, V / R
 
@@ -25,9 +27,9 @@ plt.axes([0.025, 0.025, 0.95, 0.95])
 # plt.quiver(X, Y, U, V, edgecolor='k', facecolor='None', linewidth=.5)
 plt.quiver(X, Y, U, V, R)
 
-plt.xlim(-1, n)
+plt.xlim(-1, WIDTH)
 plt.xticks(())
-plt.ylim(-1, n)
+plt.ylim(-1, HEIGHT)
 plt.yticks(())
 
 plt.show()
