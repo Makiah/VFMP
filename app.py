5ba8b59be669f7489de03cc5defeec9a755f90a9
makiahtbennett@gmail.com <makiahtbennett@gmail.com>
2018-05-05 18:03:28 -0400

    Clean up commenting

--- app.py
+++ app.py
@@ -12,45 +12,41 @@ spline_string = input("f(x) = y = ")
 spline_parsed = sympify(spline_string)
 lookahead_factor = float(input("lookahead (0-1): "))
 
-# Interpret the spline
+# Interpret the spline using "sympify"
 x = Symbol("x")
 print("Parsed function is " + str(spline_parsed))
 
-# GRAPHING ACTUAL FUNCTION
+# Graph the actual function by sampling points along x values.  
 x_vals = np.arange(0, WIDTH, .1)
 lam_x = lambdify(x, spline_parsed, modules=['numpy'])
 y_vals = lam_x(x_vals - (WIDTH - 1) / 2) + (HEIGHT - 1) / 2
 plt.plot(x_vals, y_vals)
 
-# GRAPHING VECTOR FIELD 
+# Graph the vector field (which matplotlib calls a "quiver")
 print("")
 y = Symbol("y")
 actual_equation = sympify(str(spline_parsed) + " - y")
 print("Actually using equation f(x,y) = " + str(actual_equation))
-dfdx = diff(actual_equation, x)
-dfdy = diff(actual_equation, y)
-print("df/dx = " + str(dfdx)) # AWESOME
+dfdx = diff(actual_equation, x) # partial derivative with respect to x
+dfdy = diff(actual_equation, y) # partial derivative with respect to y
+print("df/dx = " + str(dfdx)) 
 print("df/dy = " + str(dfdy))
 
+# Solve for x in terms of y
 xfory = solve(actual_equation, y)[0]
 print("x solved for y: " + str(xfory))
 
+# Solve for y in terms of x
 yforx = solve(actual_equation, x)[0] # returns multiple
 print("y solved for x: " + str(yforx))
 
 # The grid size, the arrows will be located on each of the grid points.  
 X, Y = np.mgrid[0:WIDTH, 0:HEIGHT]
 
-# Vector properties: r is color, T is angle
-# T = np.arctan2(Y - n / 2., X - n/2.)
-# R = 10 + np.sqrt((Y - n / 2.0) ** 2 + (X - n / 2.0) ** 2)
-# U, V = R * np.cos(T), R * np.sin(T)
+# Where the vector would be positioned on the x and y grid (the np.grid makes this weird)
 X_GRAPH, Y_GRAPH = X - (WIDTH - 1) / 2., Y - (HEIGHT - 1) / 2.
 
-# Define the two graph equations here, using X_GRAPH and Y_GRAPH as input parameters.  
-# u_eqn = lambdify([x, y], dfdx * actual_equation + yforx, modules=['numpy'])
-# v_eqn = lambdify([x, y], dfdy * actual_equation + xfory, modules=['numpy'])
-
+# Use calculus I figured out experimentally to determine the vector x and y components.  SymPy makes this extremely easy.  
 dfdx_lam = lambdify([x, y], dfdx, modules=['numpy'])
 dfdy_lam = lambdify([x, y], dfdy, modules=['numpy'])
 actual_lam = lambdify([x, y], actual_equation, modules=['numpy'])
@@ -65,12 +61,12 @@ U, V = U / R, V / R     # normalized x and y
 
 # Actually graph the vector field.  
 plt.quiver(X, Y, U, V, R, pivot="middle")
-# plt.quiver(X, Y, U, V, R, alpha=.5)
-# plt.quiver(X, Y, U, V, edgecolor='k', facecolor='None', linewidth=.5)
 
+# Define the width and height of the graph
 plt.xlim(0, WIDTH)
 plt.xticks(())
 plt.ylim(0, HEIGHT)
 plt.yticks(())
 
+# Show the graph
 plt.show()
