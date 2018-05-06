e80b7a150b690737ad6cb747e2b5680369c2391b
makiahtbennett@gmail.com <makiahtbennett@gmail.com>
2018-05-05 21:05:17 -0400

    Mathematical improvements

--- app.py
+++ app.py
@@ -8,37 +8,27 @@ WIDTH = 40
 HEIGHT = 30
 
 # Use the amazingness of sympy to figure out the spline to use.  
-spline_string = input("f(x) = y = ")
+spline_string = input("f(x, y) = ")
 spline_parsed = sympify(spline_string)
 lookahead_factor = float(input("lookahead (0-1): "))
 
 # Interpret the spline using "sympify"
 x = Symbol("x")
+y = Symbol("y")
 print("Parsed function is " + str(spline_parsed))
 
 # Graph the actual function by sampling points along x values.  
 x_vals = np.arange(0, WIDTH, .1)
-lam_x = lambdify(x, spline_parsed, modules=['numpy'])
+lam_x = lambdify(x, solve(spline_parsed, y)[0], modules=['numpy'])
 y_vals = lam_x(x_vals - (WIDTH - 1) / 2) + (HEIGHT - 1) / 2
 plt.plot(x_vals, y_vals)
 
 # Graph the vector field (which matplotlib calls a "quiver")
 print("")
-y = Symbol("y")
-actual_equation = sympify(str(spline_parsed) + " - y")
-print("Actually using equation f(x,y) = " + str(actual_equation))
-dfdx = diff(actual_equation, x) # partial derivative with respect to x
-dfdy = diff(actual_equation, y) # partial derivative with respect to y
+dfdx = diff(spline_parsed, x) # partial derivative with respect to x
+dfdy = diff(spline_parsed, y) # partial derivative with respect to y
 print("df/dx = " + str(dfdx)) 
 print("df/dy = " + str(dfdy))
-
-# Solve for x in terms of y
-xfory = solve(actual_equation, y)[0]
-print("x solved for y: " + str(xfory))
-
-# Solve for y in terms of x
-yforx = solve(actual_equation, x)[0] # returns multiple
-print("y solved for x: " + str(yforx))
 
 # The grid size, the arrows will be located on each of the grid points.  
 X, Y = np.mgrid[0:WIDTH, 0:HEIGHT]
@@ -47,16 +37,14 @@ X, Y = np.mgrid[0:WIDTH, 0:HEIGHT]
 X_GRAPH, Y_GRAPH = X - (WIDTH - 1) / 2., Y - (HEIGHT - 1) / 2.
 
 # Print out the vector equation components
-print("Vector field: <(" + str(dfdx) + ") * (" + str(actual_equation) + ") + (" + str(yforx) + "), (" + str(dfdy) + ") * (" + str(actual_equation) + ") + (" + str(xfory) + ")>")
+print("Vector field: < -(" + str(dfdx) + ") * (" + str(spline_parsed) + ") + -(" + str(dfdy) + "), -(" + str(dfdx) + ") * (" + str(spline_parsed) + ") + (" + str(dfdx) + ")>")
 
 # Use calculus I figured out experimentally to determine the vector x and y components.  SymPy makes this extremely easy.  
 dfdx_lam = lambdify([x, y], dfdx, modules=['numpy'])
 dfdy_lam = lambdify([x, y], dfdy, modules=['numpy'])
-actual_lam = lambdify([x, y], actual_equation, modules=['numpy'])
-yforx_lam = lambdify([x, y], yforx, modules=['numpy'])
-xfory_lam = lambdify([x, y], xfory, modules=['numpy'])
-U = lookahead_factor * (-1 * dfdx_lam(X_GRAPH, Y_GRAPH) * actual_lam(X_GRAPH, Y_GRAPH)) + (-dfdy_lam(X_GRAPH, Y_GRAPH))
-V = lookahead_factor * (-1 * dfdy_lam(X_GRAPH, Y_GRAPH) * actual_lam(X_GRAPH, Y_GRAPH)) + (dfdx_lam(X_GRAPH, Y_GRAPH))
+spline_parsed_lam = lambdify([x, y], spline_parsed, modules=['numpy'])
+U = lookahead_factor * (-dfdx_lam(X_GRAPH, Y_GRAPH) * spline_parsed_lam(X_GRAPH, Y_GRAPH)) + (-dfdy_lam(X_GRAPH, Y_GRAPH))
+V = lookahead_factor * (-dfdy_lam(X_GRAPH, Y_GRAPH) * spline_parsed_lam(X_GRAPH, Y_GRAPH)) + (dfdx_lam(X_GRAPH, Y_GRAPH))
 
 # Normalize the vector magnitudes to improve their appearance, otherwise we get weird, miniscule vectors.  
 R = np.sqrt(U**2+V**2)  # color of the vector
